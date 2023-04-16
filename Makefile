212a9cc835ede0baf8eb86c599f1ae1b92cf00f5
ybouali <yassine.bouali.bo@gmail.com>
2023-04-16 23:04:56 +0000

    DONE ADDING FONTS TO THE APP AND ADD ICON FOR THE APP

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	@flutter run 
+clean:
+	@flutter clean
--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter layout demo'),
+          title: const Text( 'My App', ),
+          leading: Image.asset('assets/images/chatgpt.png'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text('hello world !'),
         ),
       ),
     );
